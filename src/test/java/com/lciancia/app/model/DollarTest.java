63036d1146cf4815b00e8e9945ae69b26e5d4c0b
Lorenzo Cianciaruso <Lorenzo Cianciaruso>
2017-03-18 18:04:26 +0000

    cap2 - Remove side effect from Dollar

--- src/test/java/com/lciancia/app/model/DollarTest.java
+++ src/test/java/com/lciancia/app/model/DollarTest.java
@@ -13,9 +13,11 @@ public class DollarTest {
     }
 
     @Test
-    public void testTimes() throws Exception {
+    public void testMultiplication() throws Exception {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
